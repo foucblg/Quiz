aa0f410924d2dd5bf52e1b29742fc72d6d883cf1
Tristan Balandier <tristan.balandier@etu.ec-lyon.fr>
2025-02-04 17:28:56 +0100

    Clean version

---
+++ src/app/views/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
+import { ImageModule } from 'primeng/image';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [ImageModule, DividerModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
